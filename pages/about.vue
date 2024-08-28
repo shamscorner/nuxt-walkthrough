5a0c0f5e77d7fb1affe147c0b193b4c8246a126c
Shamim <shossain@secureship.ca>
2024-08-29 02:04:14 +0600

    chore: initial commit

---
+++ pages/about.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+import { ref } from "vue";
+
+const count = ref(0);
+</script>
+
+<template>
+  <div>
+    <h1>This is about page!</h1>
+    <pre>{{ count }}</pre>
+  </div>
+</template>
+
+<style></style>
