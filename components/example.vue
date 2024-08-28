9b7d38597c16330cbaf589d80094759e7d566bbe
Shamim <shossain@secureship.ca>
2024-08-29 02:32:34 +0600

    chore: add component demo

---
+++ components/example.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup>
+const example = useExample();
+</script>
+
+<template>
+  <div>
+    <!-- more like use-client on react -->
+    <ClientOnly fallback-tag="span" fallback="Loading...">
+      <pre>{{ JSON.stringify(example.config.app, null, 2) }}</pre>
+    </ClientOnly>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,7 @@
 <script setup lang="ts">
 const route = useRoute();
-const example = useExample();
 </script>
 
 <template>
-  <!-- more like use-client on react -->
-  <ClientOnly fallback-tag="span" fallback="Loading...">
-    <pre>{{ JSON.stringify(example.config.app, null, 2) }}</pre>
-  </ClientOnly>
+  <Example />
 </template>
