6eaa14f0e21e9ec5f59150c09a9afd2943c8a266
Shamim <shossain@secureship.ca>
2024-09-03 00:16:31 +0600

    chore: add app.config example

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
 const runtimeConfig = useRuntimeConfig();
+const appConfig = useAppConfig();
 </script>
 
 <template>
   <div>
     {{ runtimeConfig.public.apiBase }}
+
+    <pre>{{ JSON.stringify(appConfig, null, 2) }}</pre>
   </div>
 </template>
